7870f217f1e3cc46e567f0da94dfb3c800fa2d6f
aoddy10 <vobi69p@gmail.com>
2023-12-11 17:49:40 +1100

    Add dashboard buton that user can click to go to dashboard if the do not signout yet.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,17 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
 
 const LandingPage = () => {
+  const { userId } = auth();
+
   return (
     <>
       <div>Landing Page - Un protected.</div>
-      <div>
-        <a href="/sign-in">
-          <Button>Sign In</Button>
-        </a>
-        <a href="/sign-up">
-          <Button>Sign Up</Button>
-        </a>
-      </div>
+      {userId ? (
+        <div>
+          <Link href="/dashboard">
+            <Button>Dashboard</Button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <a href="/sign-in">
+            <Button>Sign In</Button>
+          </a>
+          <a href="/sign-up">
+            <Button>Sign Up</Button>
+          </a>
+        </div>
+      )}
     </>
   );
 };
