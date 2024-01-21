55a0e7bf9dc0cde9ac99bb8d6a5c3b1a23c06ab7
aoddy10 <vobi69p@gmail.com>
2024-01-21 12:04:41 +1100

    Create landing page.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,30 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { auth } from "@clerk/nextjs";
-import Link from "next/link";
+import LandingContent from "@/components/landing-content";
+import LandingHero from "@/components/landing-hero";
+import LandingNavbar from "@/components/ui/landing-navbar";
 
 const LandingPage = () => {
-  const { userId } = auth();
-
   return (
-    <>
-      <div>Landing Page - Un protected.</div>
-      {userId ? (
-        <div>
-          <Link href="/dashboard">
-            <Button>Dashboard</Button>
-          </Link>
-        </div>
-      ) : (
-        <div>
-          <a href="/sign-in">
-            <Button>Sign In</Button>
-          </a>
-          <a href="/sign-up">
-            <Button>Sign Up</Button>
-          </a>
-        </div>
-      )}
-    </>
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
   );
 };
 
